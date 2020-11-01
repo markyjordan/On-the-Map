59a7ebe5d928735c2d8614af97a3a3eb7fbdf393
Mark Jordan <markybjordan@gmail.com>
2020-11-01 06:24:07 -0800

    Created new project.
    Added all the default files and folders to the repo.

---
+++ On the Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ On the MapTests/On_the_MapTests.swift
@@ -0,0 +1,33 @@
+//
+//  On_the_MapTests.swift
+//  On the MapTests
+//
+//  Created by Marky Jordan on 11/1/20.
+//
+
+import XCTest
+@testable import On_the_Map
+
+class On_the_MapTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
