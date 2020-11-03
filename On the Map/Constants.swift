229b6580f4a776ef8fcda33ed121f85a05ec49f1
Mark Jordan <markybjordan@gmail.com>
2020-11-02 16:25:53 -0800

    Declared a UdacityClient class.

---
+++ On the Map/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/2/20.
+//
+
+let myUdacityAPIKey = ""
---
+++ On the Map/Model/UdacityClient.swift
@@ -0,0 +1,14 @@
+//
+//  UdacityClient.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/2/20.
+//
+
+import Foundation
+
+class UdacityClient {
+    
+    static let apiKey = myUdacityAPIKey
+    
+}
