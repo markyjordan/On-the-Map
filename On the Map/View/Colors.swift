f45a41ba0e41c85bdbc6b1e7d6281e28c8d857a9
Mark Jordan <markybjordan@gmail.com>
2020-11-07 12:56:51 -0800

    Added new file: 'Colors.swift'.
    This file extends the UIColor class. Constants were defined to allow for the creation of color gradients.

---
+++ On the Map/View/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/7/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let primaryDark = UIColor(named: "PrimaryDark")!
+    static let primaryLight = UIColor(named: "PrimaryLight")!
+    static let gradientTop = UIColor(named: "GradientTop")!
+    static let gradientBottom = UIColor(named: "GradientBottom")!
+}
