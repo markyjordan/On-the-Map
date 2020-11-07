119e57aaf8d067d5433afdd97696ef0608e87c04
Mark Jordan <markybjordan@gmail.com>
2020-11-07 13:56:04 -0800

    Created new classes.
    MapViewController and ListViewController.

---
+++ On the Map/Controller/ListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewController.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/7/20.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ On the Map/Controller/MapViewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewViewController.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/7/20.
+//
+
+import UIKit
+
+class MapViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
