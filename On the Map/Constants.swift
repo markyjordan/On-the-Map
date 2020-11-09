cd47f0a2cde0e1fd3db41ae0b0c18d7d561594b4
Mark Jordan <markybjordan@gmail.com>
2020-11-09 12:56:01 -0800

    Defined a Constants class and added a constant.
    The constants include an Udacity user api key and the url for Udacity user sign up.

--- On the Map/Constants.swift
+++ On the Map/Constants.swift
@@ -5,4 +5,10 @@
 //  Created by Marky Jordan on 11/2/20.
 //
 
-let myUdacityAPIKey = ""
+class Constants {
+    
+    static let udacityUserAPIKey = ""
+    static let udacityURLSignUp = "https://auth.udacity.com/sign-up"
+    
+}
+
