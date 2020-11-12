77cec6201b0c92aa674cabaa9e4f7ff5297d2a7c
Mark Jordan <markybjordan@gmail.com>
2020-11-11 23:04:12 -0800

    Created an object to represent the API endpoints.

--- On the Map/Model/Networking Clients/UdacityClient.swift
+++ On the Map/Model/Networking Clients/UdacityClient.swift
@@ -16,4 +16,37 @@ class UdacityClient {
         static var lastName = ""
     }
     
+    enum Endpoints {
+        static let base = "https://onthemap-api.udacity.com/v1"
+        
+        case login
+        case signUp
+        case getStudentLocation
+        case addStudentLocation
+        case updateStudentLocation
+        case getUserInformation
+        
+        var stringValue: String {
+            switch self {
+            case .login:
+                return
+            case .signUp:
+                return
+            case .getStudentLocation:
+                return
+            case .addStudentLocation:
+                return
+            case .updateStudentLocation:
+                return
+            case .getUserInformation:
+                return
+            }
+        }
+        
+        // generate a URL from the endpoint's associated string value
+        var url: URL {
+            return URL(string: stringValue)!
+        }
+    }
+    
 }
