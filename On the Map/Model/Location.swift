2bf51abaea33e7916cfd76eb45184cbe08ef2f9d
Mark Jordan <markybjordan@gmail.com>
2020-11-12 23:51:57 -0800

    Built a model for location data.

---
+++ On the Map/Model/Location.swift
@@ -0,0 +1,21 @@
+//
+//  Location.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/12/20.
+//
+
+import Foundation
+
+struct Location: Codable {
+    let objectId: String
+    let uniqueKey: String?
+    let firstName: String?
+    let lastName: String?
+    let mapString: String?
+    let mediaURL: String?
+    let latitude: Double?
+    let longitude: Double?
+    let createdAt: String
+    let updatedAt: String
+}
