db1fcdf285678f421c06fab90f30da9f57d0f23a
Mark Jordan <markybjordan@gmail.com>
2020-11-14 23:55:13 -0800

    Built model to represent user session.

---
+++ On the Map/Model/Session.swift
@@ -0,0 +1,14 @@
+//
+//  Session.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/14/20.
+//
+
+import Foundation
+
+struct Session: Codable {
+    
+    let id: String
+    let expiration: String
+}
