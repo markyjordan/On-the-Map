f5d80ddb5c376ec38a9ecbf8dcd3c146b7aadd2d
Mark Jordan <markybjordan@gmail.com>
2020-11-16 23:46:11 -0800

    Created a model for the user profile response.

---
+++ On the Map/Model/UserProfile.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfile.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/16/20.
+//
+
+import Foundation
+
+struct UserProfile {
+    let firstName: String
+    let lastName: String
+    let nickName: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case nickName
+    }
+}
