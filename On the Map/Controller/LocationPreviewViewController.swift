05e0b396150c2ab3f7c35600389875dc46da8f37
Mark Jordan <markybjordan@gmail.com>
2020-11-17 21:18:41 -0800

    Added a LocationPreviewViewController class.

---
+++ On the Map/Controller/LocationPreviewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationPreviewViewController.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/17/20.
+//
+
+import UIKit
+
+class LocationPreviewViewController: UIViewController {
+
+    // MARK: - Outlets/Properties
+    
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    
+    // MARK: - Navigation
+    
+
+
+}
