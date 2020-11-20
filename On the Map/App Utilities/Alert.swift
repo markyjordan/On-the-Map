4c21dff62128f16bb3d3ec04ca0c95e1253c8109
Mark Jordan <markybjordan@gmail.com>
2020-11-20 06:56:18 -0800

    Defined 'MapViewVC' class.

---
+++ On the Map/App Utilities/Alert.swift
@@ -0,0 +1,16 @@
+//
+//  Alert.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/20/20.
+//
+
+import Foundation
+import UIKit
+
+enum Alert {
+    
+    
+    
+    
+}
---
+++ On the Map/Controller/MapViewVC.swift
@@ -0,0 +1,50 @@
+//
+//  MapViewVC.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 11/20/20.
+//
+
+import UIKit
+import MapKit
+
+class MapViewVC: UIViewController, MKMapViewDelegate {
+
+    // MARK: - Outlets/Properties
+    
+    var mapView: MKMapView!
+    var locations = [String: Any]()
+    var annotations = [MKPointAnnotation]()
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    
+    // MARK: - MKMapViewDelegate Methods
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+
+
+}
