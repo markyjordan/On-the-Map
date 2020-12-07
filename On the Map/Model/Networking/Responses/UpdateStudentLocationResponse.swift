1df06e033294e9732dd3c4b7b1f798c4db6b20b8
Mark Jordan <markybjordan@gmail.com>
2020-12-07 07:06:07 -0800

    Created a model for UpdateStudentLocationResponse.

---
+++ On the Map/Model/Networking/Responses/UpdateStudentLocationResponse.swift
@@ -0,0 +1,12 @@
+//
+//  UpdateStudentLocationResponse.swift
+//  On the Map
+//
+//  Created by Marky Jordan on 12/7/20.
+//
+
+import Foundation
+
+struct UpdateStudentLocationResponse: Codable {
+    let updateAt: String
+}
