5497bc9d6380bd675393fe58317e2754f1f3fcff
Mark Jordan <markybjordan@gmail.com>
2020-12-07 20:59:10 -0800

    Edited the nickname property in UserProfile model.

--- On the Map/Model/Networking/Responses/UserProfile.swift
+++ On the Map/Model/Networking/Responses/UserProfile.swift
@@ -10,11 +10,11 @@ import Foundation
 struct UserProfile: Codable {
     let firstName: String
     let lastName: String
-    let nickName: String?
+    let nickname: String?
     
     enum CodingKeys: String, CodingKey {
         case firstName = "first_name"
         case lastName = "last_name"
-        case nickName
+        case nickname
     }
 }
