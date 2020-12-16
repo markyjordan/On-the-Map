db87d7ae4c4493ee2121b1fd6ea0b14fdcdf818b
Mark Jordan <markybjordan@gmail.com>
2020-12-15 21:52:05 -0800

    Added a variety of functions to the Alert type.

--- On the Map/App Utilities/Alert.swift
+++ On the Map/App Utilities/Alert.swift
@@ -5,12 +5,36 @@
 //  Created by Marky Jordan on 11/20/20.
 //
 
-import Foundation
 import UIKit
 
-enum Alert {
+struct Alert {
     
+    private static func showBasicAlert(on vc: UIViewController, title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        vc.present(alert, animated: true, completion: nil)
+    }
     
+    // failed network connection
     
+    // invalid link
+    
+    // incorrect login credentials (email and password)
+    static func showInvalidLogin(on vc: UIViewController) {
+        showBasicAlert(on: vc, title: "Log In Failed", message: "Please enter a valid email and password to continue.")
+    }
+    
+    // invalid location url
+
+    
+    // failed to forward location geocode
+    static func showLocationNotFound(on vc: UIViewController, message: Error) {
+        showBasicAlert(on: vc, title: "Location Not Found", message: message.localizedDescription)
+    }
+    
+    // failed to post geocode map/pin data to server
+    static func showFailedToPost(on vc: UIViewController) {
+        showBasicAlert(on: vc, title: "Add Location Failed", message: "Please try again later.")
+    }
     
 }
