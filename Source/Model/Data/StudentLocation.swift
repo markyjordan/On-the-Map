f6e5c83f22823f1f7f661295e1dc8b2a17f1e16d
Mark Jordan <markydotio@gmail.com>
2021-02-10 15:30:01 -0800

    Created an init method.
    Now each student location data object retrieved from the Udacity server will
    have default values for each property where the value was otherwise nil.

--- Source/Model/Data/StudentLocation.swift
+++ Source/Model/Data/StudentLocation.swift
@@ -18,4 +18,18 @@ struct StudentLocation: Codable {
     let objectId: String?
     let uniqueKey: String?
     let updatedAt: String?
+    
+    //  initialize a default value where a value for any given property is nil
+    init(fromStudentData studentLocation: [String: Any]) {
+        self.createdAt = studentLocation["createdAt"] as? String
+        self.firstName = studentLocation["firstName"] as? String ?? ""
+        self.lastName = studentLocation["lastName"] as? String ?? ""
+        self.latitude = studentLocation["latitude"] as? Double ?? 0.0
+        self.longitude = studentLocation["longitude"] as? Double ?? 0.0
+        self.mapString = studentLocation["mapString"] as? String ?? ""
+        self.mediaURL = studentLocation["mediaURL"] as? String ?? ""
+        self.objectId = studentLocation["objectId"] as? String
+        self.uniqueKey = studentLocation["uniqueKey"] as? String ?? ""
+        self.updatedAt = studentLocation["updatedAt"] as? String
+    }
 }
